15881f31dd4b7c84056d252432da02a8a359e1db
Laiamdelprado <lmarco@userlytics.com>
2022-08-07 12:13:27 +0200

    multiply and divide changed / taken from the store

--- src/app/counter/counter.reducer.ts
+++ src/app/counter/counter.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { decrement, increment } from './counter.actions';
+import { decrement, divide, increment, multiply } from './counter.actions';
 
 
 export const initialState = 10;
@@ -7,5 +7,7 @@ export const initialState = 10;
 export const counterReducer = createReducer(
   initialState,
   on(increment, (state) => state + 1),
-  on(decrement, (state) => state - 1)
+  on(decrement, (state) => state - 1),
+  on(multiply, (state) => state * 2),
+  on(divide, (state) => state / 2)
 );
--- src/app/counter/son/son.component.ts
+++ src/app/counter/son/son.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
+import * as actions from '../counter.actions';
 
 @Component({
   selector: 'app-son',
@@ -12,7 +13,11 @@ export class SonComponent implements OnInit {
 
   /*Keep in mind that the Store allows you to know the state stored,
   so thanks to the Store we know the counter value */
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>) {
+    this.store.subscribe((state) => {
+      this.counter = state.counter;
+    });
+  }
 
   ngOnInit(): void {
     this.store.select('counter')
@@ -20,12 +25,10 @@ export class SonComponent implements OnInit {
   }
 
   multiply() {
-    //this.counter *= 2;
-    //this.counterChange.emit(this.counter);
+    this.store.dispatch(actions.multiply());
   }
 
   divide() {
-    //this.counter /= 2;
-    //this.counterChange.emit(this.counter);
+    this.store.dispatch(actions.divide());
   }
 }
